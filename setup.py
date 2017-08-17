3005f492315dbfa37d8ae2975b133de223b35493
132nd-etcher <132nd-etcher@daribouca.net>
2017-08-17 14:10:30 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+"""
+Stub
+"""
+
+setup(
+    name="elib",
+    version="0.1.0",
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
