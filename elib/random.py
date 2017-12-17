b41e0f499af1255682a4961b573b5e9295d216d2
132nd-etcher <132nd-etcher@daribouca.net>
2017-12-17 23:52:01 +0100

    new: add random module

---
+++ elib/random.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+
+import random
+import string
+
+
+def random_string(size=6, chars=string.ascii_uppercase + string.digits) -> str:
+    """
+    Creates a random string
+
+    :param size: len of str
+    :param chars: character pool
+    :return: random string
+    """
+    return ''.join(random.choice(chars) for _ in range(size))
