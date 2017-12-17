45722bc7c427f8bdb6050f835aa09c7e6c3c5223
132nd-etcher <132nd-etcher@daribouca.net>
2017-12-17 05:27:28 +0100

    chg: clean setup

--- setup.py
+++ setup.py
@@ -1,85 +1,64 @@
 # coding=utf-8
 
-# import shutil
-# import os
-from setuptools import find_packages, setup
+import os
 
-dependency_links = []
+from setuptools import setup
 
-install_requires = [
-    'pyyaml',
-    'everett',
-]
+import versioneer
 
-test_requires = [
-    'pytest',
-    'pytest-pycharm',
-    'flake8',
-    'pylint',
-    'safety',
-    'coverage',
-    'pytest-pep8',
-    'pytest-cache',
-    'pytest-catchlog',
-    'pytest-cov',
-    'hypothesis',
-]
+from pip.req import parse_requirements
 
-dev_requires = [
-    'pip-tools',
-    'epab',
-]
-
-setup_requires = [
-    'setuptools_scm',
-]
-
-entry_points = ''
+requirements = [str(r.req) for r in
+                parse_requirements('requirements.txt', session=False)]
+test_requirements = [str(r.req) for r in
+                     parse_requirements('requirements-dev.txt', session=False)]
 
 
-def main():
-    try:
-        # shutil.copy2('./CHANGELOG.rst', './elib/CHANGELOG.rst')
-        # shutil.copy2('./README.md', './elib/README.md')
-        setup(
-            name='elib',
-            use_scm_version=True,
-            zip_safe=False,
-            install_requires=install_requires,
-            entry_points=entry_points,
-            tests_require=test_requires,
-            setup_requires=setup_requires,
-            dependency_links=dependency_links,
-            package_dir={'elib': 'elib'},
-            package_data={},
-            test_suite='pytest',
-            packages=find_packages(),
-            python_requires='>=3.6',
-            extras_require={
-                'dev': dev_requires,
-                'test': test_requires,
-            },
-            license='MIT',
-            classifiers=[
-                'Development Status :: 3 - Alpha',
-                'Environment :: Win32 (MS Windows)',
-                'Intended Audience :: Developers',
-                'Natural Language :: English',
-                'Operating System :: Microsoft :: Windows :: Windows 7',
-                'Operating System :: Microsoft :: Windows :: Windows 8',
-                'Operating System :: Microsoft :: Windows :: Windows 8.1',
-                'Operating System :: Microsoft :: Windows :: Windows 10',
-                'License :: OSI Approved :: MIT License',
-                'Programming Language :: Python :: 3.6',
-                'Programming Language :: Python :: 3.7',
-                'Topic :: Utilities',
-            ],
-        )
-    finally:
-        # os.remove('./elib/CHANGELOG.rst')
-        # os.remove('./elib/README.md')
-        pass
+def read_local_files(*file_paths: str) -> str:
+    """
+    Reads one or more text files and returns them joined together.
+
+    A title is automatically created based on the file name.
+
+    :param file_paths: list of files to aggregate
+
+    """
+
+    def _read_single_file(file_path):
+        with open(file_path) as f:
+            filename = os.path.splitext(file_path)[0]
+            title = f'{filename}\n{"=" * len(filename)}'
+            return '\n\n'.join((title, f.read()))
+
+    return '\n' + '\n\n'.join(map(_read_single_file, file_paths))
 
 
-if __name__ == '__main__':
-    main()
+setup(
+    name='elib',
+    use_scm_version=True,
+    zip_safe=False,
+    install_requires=requirements,
+    tests_require=test_requirements,
+    package_dir={'elib': 'elib'},
+    package_data={},
+    test_suite='pytest',
+    packages=['elib'],
+    python_requires='>=3.6',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows :: Windows 7',
+        'Operating System :: Microsoft :: Windows :: Windows 8',
+        'Operating System :: Microsoft :: Windows :: Windows 8.1',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+    ],
+)
