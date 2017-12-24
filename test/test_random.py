1ba6db5180d6e39a13e8ce67bc73063f1af54ad4
132nd-etcher <132nd-etcher@daribouca.net>
2017-12-24 12:07:39 +0100

    new: add tests for random utils

---
+++ test/test_random.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+from hypothesis import strategies as st, given
+
+from elib.custom_random import random_string, random_bytes
+
+
+def test_random_str():
+    res = set()
+    for _ in range(10000):
+        rand = random_string()
+        assert len(rand) == 6
+        assert rand not in res
+        res.add(rand)
+
+
+def test_random_bytes():
+    res = set()
+    for _ in range(100000):
+        rand = random_bytes()
+        assert len(rand) == 1024
+        assert rand not in res
+        res.add(rand)
+
+
+@given(length=st.integers(min_value=1, max_value=1024))
+def test_random_string_length(length):
+    assert len(random_string(length)) == length
+
