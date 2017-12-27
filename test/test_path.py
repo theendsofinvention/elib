5d299e7b6b9f42a8a7357d8c06342ec2df6a692c
132nd-etcher <132nd-etcher@daribouca.net>
2017-12-27 06:14:04 +0100

    new: dev: add tests for paths

---
+++ test/test_path.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""
+Tests path package
+"""
+
+import pytest
+
+from pathlib import Path
+
+import elib.path
+
+
+def test_path():
+    path_str = './test'
+    path_test = Path('./test')
+    assert elib.path.ensure_path(path_str, must_exit=False) == elib.path.ensure_path(path_test, must_exit=False)
+    with pytest.raises(FileNotFoundError):
+        elib.path.ensure_path(path_str)
+    with pytest.raises(FileNotFoundError):
+        elib.path.ensure_path(path_test)
