7326675187d77ac47e243bc14c16e1620df0068a
132nd-etcher <132nd-etcher@daribouca.net>
2018-01-02 16:58:34 +0100

    fix: linting

--- elib/console.py
+++ elib/console.py
@@ -7,4 +7,10 @@ import ctypes
 
 
 def set_title(title: str):
+    """
+    Sets the console title
+
+    Args:
+        title: title as a string
+    """
     ctypes.windll.kernel32.SetConsoleTitleW(title)
