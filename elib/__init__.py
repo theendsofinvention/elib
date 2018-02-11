659e8e26449b10cd7339e6880012379c99a4f3c8
132nd-etcher <132nd-etcher@users.noreply.github.com>
2018-02-11 19:04:25 +0100

    fix: fix wrong package name in version (#6)

--- elib/__init__.py
+++ elib/__init__.py
@@ -6,7 +6,7 @@ from pkg_resources import DistributionNotFound, get_distribution
 from . import config, console, custom_logging, custom_random, paste, path, tts
 
 try:
-    __version__ = get_distribution('esst').version
+    __version__ = get_distribution('elib').version
 except DistributionNotFound:  # pragma: no cover
     # package is not installed
     __version__ = 'not installed'
