27c765e47c2a846893f375faa5da359529c6f344
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-14 15:42:35 +0100

    add pretty format with indent

--- elib/__init__.py
+++ elib/__init__.py
@@ -5,6 +5,7 @@ from pkg_resources import DistributionNotFound, get_distribution
 
 from . import config, console, custom_logging, custom_random, paste, path, tts
 from ._run import run
+from .pretty import pretty_format
 
 try:
     __version__ = get_distribution('elib').version
---
+++ test/test_pretty.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+
+import elib
+
+def test_pretty():
+    obj = {
+        'test': 'value',
+        'nested': {
+            'test': 'value'
+        },
+        'a': 't',
+        'b': 't',
+        'c': 't',
+        'd': 't',
+        'e': 't',
+        'f': 't',
+        'g': 't',
+        'h': 't',
+        'i': 't',
+        'j': 't',
+        'k': 't',
+        'l': 't',
+    }
+    out = elib.pretty_format(obj)
+    print(out)
+    assert out == """    {'a': 't',
+     'b': 't',
+     'c': 't',
+     'd': 't',
+     'e': 't',
+     'f': 't',
+     'g': 't',
+     'h': 't',
+     'i': 't',
+     'j': 't',
+     'k': 't',
+     'l': 't',
+     'nested': {'test': 'value'},
+     'test': 'value'}"""
+
+
+def test_pretty_no_indent():
+    obj = {
+        'test': 'value'
+    }
+    out = elib.pretty_format(obj, indent=0)
+    assert out == "{'test': 'value'}"
