9e5ff06a8fd9144d0137ae049137f465f8df4c59
132nd-etcher <132nd-etcher@users.noreply.github.com>
2018-02-14 15:54:19 +0100

    new: add pretty format with indent (#15)
    * add pretty format with indent
    
    * linting

---
+++ elib/pretty.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+"""
+Formats an object for representation on the console or in a log
+"""
+
+
+import pprint
+
+
+def _indent(text, indent=4):
+    fstring = ' ' * indent + '{}'
+    return ''.join([fstring.format(l) for l in text.splitlines(True)])
+
+
+def pretty_format(obj, indent=4):
+    """
+    Formats an object for representation on the console or in a log
+
+    Args:
+        obj: object to represent
+        indent: indent (defaults to 4)
+
+    Returns: formatted text
+
+    """
+    return _indent(pprint.pformat(obj), indent=indent)
