bae3d9c0fcfe8c936b56429743a3c7c6f0a55fb1
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-16 10:51:24 +0100

    fix tests

--- test/test_hash.py
+++ test/test_hash.py
@@ -2,20 +2,20 @@
 from pathlib import Path
 
 import pytest
-from elib import get_hash
+import elib
 
 
 def test_get_hash_from_string():
     test_string = 'this is some dummy test string'
-    assert get_hash(test_string) == 'f4c8f848b02d640c75a2385fd315412b'
-    assert get_hash(test_string, 'sha256') == '556d2c0500f5e1e40925e65d16cc3c46006006e88ffc5051fb3a7f7c1b6af9b1'
+    assert elib.hash_.get_hash(test_string) == 'f4c8f848b02d640c75a2385fd315412b'
+    assert elib.hash_.get_hash(test_string, 'sha256') == '556d2c0500f5e1e40925e65d16cc3c46006006e88ffc5051fb3a7f7c1b6af9b1'
 
 
 def test_get_hash_from_file():
     test_file = Path('./test_file')
     test_file.write_text('this is some dummy test string')
-    assert get_hash(test_file.read_bytes()) == 'f4c8f848b02d640c75a2385fd315412b'
-    assert get_hash(
+    assert elib.hash_.get_hash(test_file.read_bytes()) == 'f4c8f848b02d640c75a2385fd315412b'
+    assert elib.hash_.get_hash(
         test_file.read_bytes(),
         'sha256'
     ) == '556d2c0500f5e1e40925e65d16cc3c46006006e88ffc5051fb3a7f7c1b6af9b1'
@@ -23,4 +23,4 @@ def test_get_hash_from_file():
 
 def test_get_hash_wrong_method():
     with pytest.raises(RuntimeError):
-        get_hash('test', 'nope')
+        elib.hash_.get_hash('test', 'nope')
