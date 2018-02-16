0090ae5497a8bcdc11c568b5bb1b7696ad6cc9b8
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-16 11:28:32 +0100

    fix tests

--- test/test_download.py
+++ test/test_download.py
@@ -2,48 +2,47 @@
 from pathlib import Path
 
 import requests
-from elib import download
-from elib.downloader import Downloader
+from elib import downloader
 from mockito import mock, verify, when
 
 URL = r'http://www.ovh.net/files/1Mio.dat'
 
 
 def test_download():
-    assert download(url=URL, outfile='./test', hexdigest='6cb91af4ed4c60c11613b75cd1fc6116')
+    assert downloader.download(url=URL, outfile='./test', hexdigest='6cb91af4ed4c60c11613b75cd1fc6116')
     assert Path('./test').exists()
 
 
 def test_download_wrong_digest():
-    assert not download(url=URL, outfile='./test', hexdigest='nope')
+    assert not downloader.download(url=URL, outfile='./test', hexdigest='nope')
     assert not Path('./test').exists()
 
 
 def test_download_no_digest():
-    assert download(url=URL, outfile='./test')
+    assert downloader.download(url=URL, outfile='./test')
     assert Path('./test').exists()
 
 
 def test_download_no_data():
-    when(Downloader).download_to_memory(...)
-    assert not download(url=URL, outfile='./test', hexdigest='6cb91af4ed4c60c11613b75cd1fc6116')
-    verify(Downloader).download_to_memory(...)
+    when(downloader.Downloader).download_to_memory(...)
+    assert not downloader.download(url=URL, outfile='./test', hexdigest='6cb91af4ed4c60c11613b75cd1fc6116')
+    verify(downloader.Downloader).download_to_memory(...)
     assert not Path('./test').exists()
 
 
 def test_download_to_memory_no_data():
-    when(Downloader)._create_response().thenReturn(None)
-    assert not download(url=URL, outfile='./test', hexdigest='6cb91af4ed4c60c11613b75cd1fc6116')
-    verify(Downloader)._create_response()
+    when(downloader.Downloader)._create_response().thenReturn(None)
+    assert not downloader.download(url=URL, outfile='./test', hexdigest='6cb91af4ed4c60c11613b75cd1fc6116')
+    verify(downloader.Downloader)._create_response()
     assert not Path('./test').exists()
 
 
 def test_download_delete_failed():
     Path('./test').touch()
     assert Path('./test').exists()
-    when(Downloader)._create_response().thenReturn(None)
-    assert not download(url=URL, outfile='./test', hexdigest='6cb91af4ed4c60c11613b75cd1fc6116')
-    verify(Downloader)._create_response()
+    when(downloader.Downloader)._create_response().thenReturn(None)
+    assert not downloader.download(url=URL, outfile='./test', hexdigest='6cb91af4ed4c60c11613b75cd1fc6116')
+    verify(downloader.Downloader)._create_response()
     assert not Path('./test').exists()
 
 
@@ -52,6 +51,6 @@ def test_download_request_failed():
     req.ok = False
     req.reason = 'nope'
     when(requests).head(...).thenReturn(req)
-    assert not download(url=URL, outfile='./test', hexdigest='6cb91af4ed4c60c11613b75cd1fc6116')
+    assert not downloader.download(url=URL, outfile='./test', hexdigest='6cb91af4ed4c60c11613b75cd1fc6116')
     verify(requests).head(...)
     assert not Path('./test').exists()
