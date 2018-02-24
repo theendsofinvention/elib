8408413b8027c65e6dc9dee13725d04b90aafdea
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-24 19:58:48 +0100

    rename a few things

--- test/test_find_exe.py
+++ test/test_find_exe.py
@@ -3,30 +3,30 @@
 import sys
 from pathlib import Path
 
-import elib._run
+import elib.run
 from elib.settings import ELIBSettings
 
 
 def test_find_executable():
-    python = elib._run.find_executable('python')
-    assert elib._run.find_executable('python.exe') == python
-    assert elib._run.find_executable('python', f'{sys.prefix}/Scripts') == python
-    assert elib._run.find_executable('__sure__not__') is None
+    python = elib.run.find_executable('python')
+    assert elib.run.find_executable('python.exe') == python
+    assert elib.run.find_executable('python', f'{sys.prefix}/Scripts') == python
+    assert elib.run.find_executable('__sure__not__') is None
 
 
 def test_context():
-    assert elib._run.find_executable('__sure__not__') is None
+    assert elib.run.find_executable('__sure__not__') is None
     ELIBSettings.known_executables['__sure__not__.exe'] = 'ok'
-    assert elib._run.find_executable('__sure__not__') == 'ok'
+    assert elib.run.find_executable('__sure__not__') == 'ok'
 
 
 def test_paths():
-    assert elib._run.find_executable('python')
-    assert elib._run.find_executable('python', '.')
+    assert elib.run.find_executable('python')
+    assert elib.run.find_executable('python', '.')
     ELIBSettings.known_executables = {}
-    assert elib._run.find_executable('python', '.') is None
+    assert elib.run.find_executable('python', '.') is None
 
 
 def test_immediate():
     Path('./test.exe').touch()
-    assert elib._run.find_executable('test')
+    assert elib.run.find_executable('test')
