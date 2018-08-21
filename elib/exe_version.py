c6f41fa476569e6203c8b17b0715e6b473aeb8e7
132nd-etcher <132nd-etcher@daribouca.net>
2018-08-21 09:54:03 +0200

    fix: fix exe_version with newest pefile

--- elib/exe_version.py
+++ elib/exe_version.py
@@ -2,6 +2,7 @@
 """
 Get version info from executable
 """
+import traceback
 import typing
 from pathlib import Path
 
@@ -58,12 +59,25 @@ def get_product_version(path: typing.Union[str, Path]) -> VersionInfo:  # pragma
     path = Path(path).absolute()
     pe_info = pefile.PE(str(path))
 
-    for file_info in pe_info.FileInfo:
-        if file_info.Key == b'StringFileInfo':
-            for string in file_info.StringTable:
-                if b'FileVersion' in string.entries.keys():
-                    file_version = string.entries[b'SpecialBuild'].decode('utf8')
-                    full_version = string.entries[b'PrivateBuild'].decode('utf8')
-                    return VersionInfo(file_version, full_version)
+    try:
+        for file_info in pe_info.FileInfo:  # pragma: no branch
+            if isinstance(file_info, list):
+                for file_info_ in file_info:
+                    if file_info_.Key == b'StringFileInfo':  # pragma: no branch
+                        for string in file_info_.StringTable:  # pragma: no branch
+                            if b'FileVersion' in string.entries.keys():  # pragma: no branch
+                                file_version = string.entries[b'SpecialBuild'].decode('utf8')
+                                full_version = string.entries[b'PrivateBuild'].decode('utf8')
+                                return VersionInfo(file_version, full_version)
+            else:
+                if file_info.Key == b'StringFileInfo':  # pragma: no branch
+                    for string in file_info.StringTable:  # pragma: no branch
+                        if b'FileVersion' in string.entries.keys():  # pragma: no branch
+                            file_version = string.entries[b'SpecialBuild'].decode('utf8')
+                            full_version = string.entries[b'PrivateBuild'].decode('utf8')
+                            return VersionInfo(file_version, full_version)
 
-    raise RuntimeError(f'unable to obtain version from {path}')
+        raise RuntimeError(f'unable to obtain version from {path}')
+    except (KeyError, AttributeError) as exc:
+        traceback.print_exc()
+        raise RuntimeError(f'unable to obtain version from {path}') from exc
